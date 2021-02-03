18f649cedf9b97dc02119bee878f4f93f035f250
purpleapples <tjdvkf19@gmail.com>
2021-02-03 17:33:44 +0900

    add comment

--- file_transfer.py
+++ file_transfer.py
@@ -1,33 +1,10 @@
-from flask import Flask, Request, request
+from flask import Flask, request
 from bokeh.embed import json_item
 from flask_cors import CORS
 from pymongo import MongoClient
 
 app = Flask(__name__)
 CORS(app)
-
-
-@app.route('/test/data')
-def transfort_data():
-    import json
-    with open('./data/file_json.json', 'r') as f:
-        item = json.load(f)
-
-    return item
-
-@app.route('/test/data2')
-def load_visualization_data():
-    client = MongoClient("mongodb://issueWriter:final@18.191.252.101", 27017)
-    db = client.issue_writer
-    collection = db.visual_data
-    return {'data':  list(collection.find({}, projection={"_id": 0}).limit(1))}
-
-@app.route('/visual/date/cnt')
-def getDateCntGraph():
-    client = MongoClient("mongodb://issueWriter:final@18.191.252.101", 27017)
-    db = client.issue_writer
-    collection = db.visual_data
-    return {'data':  list(collection.find({'graph_sort': 'data_count_graph'}, projection={"_id": 0}).limit(1))}
 
 @app.route('/visual', methods=['POST'])
 def getVariableAndPutBokeh():
@@ -59,7 +36,8 @@ def getVariableAndPutBokeh():
                                 projection={"_id": 0} # objectId 제외
                                 )
                 )
-    # temporary data diet
+    # 임시로 데이터 다이어트
+    # 데이터는 반드시 하나씩 뽑혀야 합니다.
     data = data[0]
     return {'data':  data}
 
