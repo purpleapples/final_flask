e93ac9b139dcb1ccfa7ddab52857ecbcfd7bd294
purpleapples <tjdvkf19@gmail.com>
2021-02-13 17:41:14 +0900

    temporary terminate backend function

--- file_transfer.py
+++ file_transfer.py
@@ -15,25 +15,16 @@ def getVariableAndPutBokeh():
     :return: 통계시각화데이터
     """
     json_data = request.get_json()# 전송한 json data
-    print("data 확인", json_data)
-    # 년도
-    year = json_data['year']
-    # 월
-    month = json_data['month']
+    print("show bokeh find condition", json_data)
 
-    # 주차
-    week = json_data['week']
-    # graph 종류
-    graphSort = json_data['graphSort']
-
-    client = MongoClient("mongodb://issueWriter:final@18.191.252.101", 27017)
+    client = MongoClient("mongodb://issueWriter:final@121.138.83.113", 27017)
     db = client.issue_writer
-    collection = db.test
+    collection = db.visual_compParts
     # $regex :년도 닮음 검색
 
-    find_condition = {key: str(value) for key, value in json_data.items()}
-    print('find_condition', find_condition)
-    data = collection.find_one(find_condition, projection={"_id": 0}) # objectId 제외
+    data = collection.find_one(json_data, projection={"_id": 0}) # objectId 제외
+
+    print('bokeh search result : ', data)
     empty = False
     if data is None:
         empty = True
@@ -47,41 +38,31 @@ def getLdaTable():
     import numpy as np
 
     json_data = request.get_json()# 전송한 json data
-    print("data 확인", json_data)
-    # 년도
-    year = json_data['year']
-    # 월
-    month = json_data['month']
 
-    # 주차
-    week = json_data['week']
-    # graph 종류
-    graphSort = json_data['graphSort']
+    print("show lda table find condition", json_data)
 
-    client = MongoClient("mongodb://issueWriter:final@18.191.252.101", 27017)
+    client = MongoClient("mongodb://issueWriter:final@18.223.118.236", 27017)
     db = client.issue_writer
-    collection = db.lda_df_compParts
+    collection = db.Lda_compParts
     # $regex :년도 닮음 검색
 
-    find_condition = {key: str(value) for key, value in json_data.items()}
-    print('find_condition', find_condition)
-    data = list(collection.find(find_condition,
+    data = list(collection.find(json_data,
                                 projection={"_id": 0,
                                             'datetime': 1,
                                             "Perc_Contribution": 1,
-                                            'category3': 1,
-                                            "content": 1
+                                            'label': 1,
+                                            "board_content": 1
                                             }# objectId 제외
                                 )
                 )
-
+    print('lda table search result : ', data)
     # react-bootstrap-table-next를 위한 인조 key 생성
     for index, row in enumerate(data):
         row['key_value'] = index
 
     # 검색결과 없을 경우
     empty = False
-    if data is None:
+    if data == []:
         empty = True
 
     data = {'empty': empty, 'product': data}
@@ -95,23 +76,14 @@ def getModelTable():
     import numpy as np
 
     json_data = request.get_json()# 전송한 json data
-    print("request confirm", json_data)
-    # 년도
-    year = json_data['year']
-    # 월
-    month = json_data['month']
-    # 주차
-    week = json_data['week']
+    print("show model table find condition", json_data)
 
-
-    client = MongoClient("mongodb://issueWriter:final@18.191.252.101", 27017)
+    client = MongoClient("mongodb://issueWriter:final@18.223.118.236", 27017)
     db = client.issue_writer
     collection = db.LSTM_label_compParts
     # $regex :년도 닮음 검색
 
-    find_condition = {key: str(value) for key, value in json_data.items()}
-    print('find_condition', find_condition)
-    data = list(collection.find(find_condition,
+    data = list(collection.find(json_data,
                                 projection={"_id": 0,
                                             'datetime': 1,
                                             'label': 1,
@@ -122,19 +94,41 @@ def getModelTable():
                 )
     # 임시로 데이터 다이어트
     # 데이터는 반드시 하나씩 뽑혀야 합니다.
-    print('search result : ', data)
+    print('model table search result : ', data)
     for index, row in enumerate(data):
         row['key_value'] = index
 
     # 검색 결과 없을 경우
     empty = False
-    if data is None:
+    if data == []:
         empty = True
 
     product = data
     data = {'empty': empty, 'product': product}
 
     return jsonify({'data':  data})
+
+# -------------------------------------------- test ----------------------------------------------------
+@app.route('/test/visual')
+def getTestBokeh():
+    import numpy as np
+    import pandas as pd
+    import pandas_bokeh
+
+    """
+    :param YearweekNum: 년도+주차숫자
+    :return: 통계시각화데이터
+    """
+    client = MongoClient("mongodb://issueWriter:final@121.138.83.113", 27017)
+    db = client.issue_writer
+    collection = db.visual_compParts
+    # $regex :년도 닮음 검색
+
+    plot = collection.find_one({}, projection={"_id": 0}) # objectId 제
+
+    return jsonify({'data':  plot})
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
 
